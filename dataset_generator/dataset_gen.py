cd93e8753e2a7b55616d73a4c9a1b836cb51edcd
Alexandr <knxandr@rambler.ru>
2022-04-03 03:44:17 +0500

    DATASET!

---
+++ dataset_generator/dataset_gen.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import cv2
+import imutils
+import time
+import random
+
+
+def random_img(path):
+  l = os.listdir(path)
+  ll = l[random.randint(0, len(l) - 1)]
+  return cv2.imread('{}{}'.format(path, ll), cv2.IMREAD_UNCHANGED)
+
+
+def random_size(img, s_min=0.8, s_max=1.3):
+  out = imutils.resize(img, width = int(img.shape[1] * random.uniform(s_min, s_max)))
+  return out
+
+
+def adjust_gamma(img, gamma=1.0):
+  invGamma = 1.0 / gamma
+  table = np.array([((i / 255.0) ** invGamma) * 255
+    for i in np.arange(0, 256)]).astype("uint8")
+  return cv2.LUT(img, table)
+
+
+def random_position(x_l, y_l):
+  return random.randint(x_l[0], x_l[1]), random.randint(y_l[0], y_l[1])
+
+
+def combine_imgs(img1, img2, mask, x, y):
+  mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+  h1, w1 = img1.shape[:2]
+  h2, w2 = img2.shape[:2]
+  x11, x12 = np.clip(x - w2 // 2, 0, w1 - 1), np.clip(x + w2 // 2, 0, w1 - 1)
+  y11, y12 = np.clip(y - h2 // 2, 0, h1 - 1), np.clip(y + h2 // 2, 0, h1 - 1)
+  x21 = x11 - (x - w2 // 2)
+  y21 = y11 - (y - h2 // 2)
+  x22 = np.clip(x21 + x12 - x11, 0, w2)
+  y22 = np.clip(y21 + y12 - y11, 0, h2)
+  out = img1.copy()
+
+  alpha = mask[y21 : y22, x21 : x22].astype(float) / 255
+  foreground = cv2.multiply(alpha, img2[y21 : y22, x21 : x22].astype(float))
+  background = cv2.multiply(1.0 - alpha, out[y11 : y12, x11 : x12].astype(float))
+  out[y11 : y12, x11 : x12] = cv2.add(foreground, background)
+  return out
+
+
+def combine_masks(img1, mask, x, y):
+  h1, w1 = img1.shape[:2]
+  h2, w2 = mask.shape[:2]
+  x11, x12 = np.clip(x - w2 // 2, 0, w1 - 1), np.clip(x + w2 // 2, 0, w1 - 1)
+  y11, y12 = np.clip(y - h2 // 2, 0, h1 - 1), np.clip(y + h2 // 2, 0, h1 - 1)
+  x21 = x11 - (x - w2 // 2)
+  y21 = y11 - (y - h2 // 2)
+  x22 = np.clip(x21 + x12 - x11, 0, w2)
+  y22 = np.clip(y21 + y12 - y11, 0, h2)
+  out = img1.copy()
+
+
+  out[y11 : y12, x11 : x12] = cv2.bitwise_or(out[y11 : y12, x11 : x12], mask[y21 : y22, x21 : x22])
+  return out
+
+
+def generate(obj_path, back_path, output_path,
+             result_count=1000,
+             result_shape=(4000, 4000),
+             obj_num=(5, 15), obj_size=(0.7, 1.3), obj_gamma=(0.9, 1.1), obj_rotation=(-5, 5),
+             back_num=(20, 40), back_size=(0.9, 1.3), back_gamma=(0.9, 1.1), back_rotation=(-5, 5)):
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        
+    while len(os.listdir(output_path)) < result_count * 2:
+        result = cv2.bitwise_not(np.zeros((result_shape[1], result_shape[0], 3), np.uint8))
+        result_mask = np.zeros((result_shape[1], result_shape[0]), np.uint8)
+
+        ''' Background '''
+        for back_count in range(random.randint(back_num[0], back_num[1])):
+            back_img = random_img(back_path)
+            back_img = random_size(back_img, back_size[0], back_size[1])
+            
+            if random.randint(0, 1) == 1:
+              back_img = cv2.flip(back_img, random.randint(-1, 1))
+              
+            back_img = imutils.rotate(back_img, random.randint(back_rotation[0], back_rotation[1]))
+            gamma = random.uniform(back_gamma[0], back_gamma[1])
+            back_img = adjust_gamma(back_img, gamma)
+
+            o_x, o_y = random_position((0, result_shape[0]),
+                                       (0, result_shape[1]))
+
+            result = combine_imgs(result, back_img[:,:,:3], back_img[:,:,3], o_x, o_y)
+
+        ''' Objects'''
+        for obj_count in range(random.randint(obj_num[0], obj_num[1])):
+            obj_img = random_img(obj_path)
+            obj_img = random_size(obj_img, obj_size[0], obj_size[1])
+            
+            if random.randint(0, 1) == 1:
+              obj_img = cv2.flip(obj_img, random.randint(-1, 1))
+              
+            obj_img = imutils.rotate(obj_img, random.randint(obj_rotation[0], obj_rotation[1]))
+            gamma = random.uniform(obj_gamma[0], obj_gamma[1])
+            obj_img = adjust_gamma(obj_img, gamma)
+
+            o_x, o_y = random_position((0, result_shape[0]),
+                                       (0, result_shape[1]))
+
+            result = combine_imgs(result, obj_img[:,:,:3], obj_img[:,:,3], o_x, o_y)
+            result_mask = combine_masks(result_mask, obj_img[:,:,3], o_x, o_y)
+
+        name = time.time()
+        cv2.imwrite('{}{}.png'.format(output_path, name), result)
+        cv2.imwrite('{}{}_mask.png'.format(output_path, name), result_mask)
+            
+        
+    ##    result = cv2.resize(result, (1000, 1000), interpolation = cv2.INTER_AREA)
+    ##    cv2.imshow('result', result)
+    ##    result_mask = cv2.resize(result_mask, (1000, 1000), interpolation = cv2.INTER_AREA)
+    ##    cv2.imshow('result_mask', result_mask)
+    ##    cv2.waitKey(0)
+    ##    cv2.destroyAllWindows()
+
+
+
+
+
+obj_path = 'output_objects/'
+back_path = 'output_objects/'
+output_path = 'results/dataset_1/'
+
+generate(obj_path, back_path, output_path, result_count=500)
--- dataset_generator/prepare_data.py
+++ dataset_generator/prepare_data.py
@@ -15,58 +15,124 @@ def save_transparent_img(img, mask, path):
 
     return result
 
+def get_objects(path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for file in os.listdir(path):
+        if 'mask' in file:
+            img = cv2.imread('{}{}'.format(path, file))
+            img_rgb = cv2.imread('{}{}.jpg'.format(path, file[:-len('_mask.jpg')]))
+            name = file[:-len('_mask.jpg')]
+
+            H, W = img.shape[:2]
+
+            kernel = np.ones((5, 5), np.uint8)
+            img = cv2.erode(img, kernel, iterations=3)
+
+            contours, h = cv2.findContours(img[:,:,0],
+                                       cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+            
+            cnts = imutils.grab_contours((contours, h))
+            
+            for contour_ind in range(len(contours)):
+                if cv2.contourArea(contours[contour_ind]) > 80:
+                    (x, y, w, h) = cv2.boundingRect(cnts[contour_ind])
+
+                    img_s = np.zeros((H, W, 3), np.uint8)
+                    cv2.drawContours(image=img_s,
+                                     contours=contours,
+                                     contourIdx=contour_ind,
+                                     color=(255, 255, 255),
+                                     thickness=-1,
+                                     lineType=cv2.LINE_AA)
+
+                    img_s = img_s[y : y + h, x : x + w]
+                    img_s = cv2.dilate(img_s, kernel, iterations=3)
+                    img_s_rgb = img_rgb[y : y + h, x : x + w].copy()               
+
+                    save_transparent_img(img_s_rgb, img_s,
+                                         '{}{}_{}.png'.format(output_path, name,
+                                                              contour_ind))
+
+
+
+def get_back(path, output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    kernel = np.ones((5, 5), np.uint8)
+    
+    for file in os.listdir(path):
+        if 'mask' in file:
+            img = cv2.imread('{}{}'.format(path, file))
+            img_rgb = cv2.imread('{}{}.jpg'.format(path, file[:-len('_mask.jpg')]))
+            name = file[:-len('_mask.jpg')]
+
+            ret, mask = cv2.threshold(cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY),
+                                      230, 255, cv2.THRESH_BINARY_INV)
+            mask = cv2.dilate(mask, kernel, iterations=1)
+            mask = cv2.medianBlur(mask, 15)
+            mask = cv2.erode(mask, kernel, iterations=3)
+            mask = cv2.medianBlur(mask, 15)
+
+            img = cv2.dilate(img, kernel, iterations=6)
+            img = cv2.medianBlur(img, 5)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #print(mask.shape, img.shape)
+            img = mask - img
+
+            H, W = img.shape[:2]
+
+            
+            img = cv2.erode(img, kernel, iterations=10)
+            img = cv2.dilate(img, kernel, iterations=6)
+
+            contours, h = cv2.findContours(img,
+                                       cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+            
+            cnts = imutils.grab_contours((contours, h))
+            
+            for contour_ind in range(len(contours)):
+                if cv2.contourArea(contours[contour_ind]) > 80:
+                    (x, y, w, h) = cv2.boundingRect(cnts[contour_ind])
+
+                    img_s = np.zeros((H, W, 3), np.uint8)
+                    cv2.drawContours(image=img_s,
+                                     contours=contours,
+                                     contourIdx=contour_ind,
+                                     color=(255, 255, 255),
+                                     thickness=-1,
+                                     lineType=cv2.LINE_AA)
+
+                    img_s = img_s[y : y + h, x : x + w]
+                    img_s = cv2.dilate(img_s, kernel, iterations=3)
+                    img_s_rgb = img_rgb[y : y + h, x : x + w].copy()               
+
+                    save_transparent_img(img_s_rgb, img_s,
+                                         '{}{}_{}.png'.format(output_path, name,
+                                                              contour_ind))
+
+##                    img_s_rgb = cv2.bitwise_and(img_s_rgb, img_s)
+##                    cv2.imshow('{}'.format(contour_ind), img_s_rgb)
+           
+
+##            img = cv2.resize(img, (1920, 1080), interpolation = cv2.INTER_AREA)
+##            img_rgb = cv2.resize(img_rgb, (1920, 1080), interpolation = cv2.INTER_AREA)
+##            mask = cv2.resize(mask, (1920, 1080), interpolation = cv2.INTER_AREA)
+##            cv2.imshow('mask', img)
+##            cv2.imshow('rgb', img_rgb)
+##            cv2.imshow('mask1', mask)
+##            cv2.waitKey(0)
+##            break
+##    cv2.destroyAllWindows()
+
 
 path = '../tissue-segment/tissue-segment/'
 output_path = 'output_objects/'
+#get_objects(path, output_path)
 
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-
-for file in os.listdir(path):
-    if 'mask' in file:
-        img = cv2.imread('{}{}'.format(path, file))
-        img_rgb = cv2.imread('{}{}.jpg'.format(path, file[:-len('_mask.jpg')]))
-        name = file[:-len('_mask.jpg')]
-
-        H, W = img.shape[:2]
-
-        kernel = np.ones((5, 5), np.uint8)
-        img = cv2.erode(img, kernel, iterations=3)
-
-        contours, h = cv2.findContours(img[:,:,0],
-                                   cv2.RETR_EXTERNAL,
-                                   cv2.CHAIN_APPROX_SIMPLE)
-        
-        cnts = imutils.grab_contours((contours, h))
-        
-        for contour_ind in range(len(contours)):
-            if cv2.contourArea(contours[contour_ind]) > 80:
-                (x, y, w, h) = cv2.boundingRect(cnts[contour_ind])
-
-                img_s = np.zeros((H, W, 3), np.uint8)
-                cv2.drawContours(image=img_s,
-                                 contours=contours,
-                                 contourIdx=contour_ind,
-                                 color=(255, 255, 255),
-                                 thickness=-1,
-                                 lineType=cv2.LINE_AA)
-
-                img_s = img_s[y : y + h, x : x + w]
-                img_s = cv2.dilate(img_s, kernel, iterations=3)
-                img_s_rgb = img_rgb[y : y + h, x : x + w].copy()               
-
-                save_transparent_img(img_s_rgb, img_s,
-                                     '{}{}_{}.png'.format(output_path, name,
-                                                          contour_ind))
-
-                #img_s_rgb = cv2.bitwise_and(img_s_rgb, img_s)
-                #cv2.imshow('{}'.format(contour_ind), img_s_rgb)
-       
-
-        #img = cv2.resize(img, (1920, 1080), interpolation = cv2.INTER_AREA)
-        #cv2.imshow('mask', img)
-        #cv2.imshow('rgb', img_rgb)
-        #cv2.waitKey(0)
-        #break
-
-cv2.destroyAllWindows()
+output_path = 'output_back/'
+get_back(path, output_path)
