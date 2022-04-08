100ce776081ecfde0780dd1396737293a75577ee
Alexandr <knxandr@rambler.ru>
2022-04-08 13:57:05 +0500

    Create remove_dot.py

---
+++ dataset_generator/remove_dot.py
@@ -0,0 +1,14 @@
+import os
+
+output_path = 'results/dataset_1/'
+images_output_path = '{}/images/'.format(output_path)
+annotation_output_path = '{}/annotation/'.format(output_path)
+
+def foo(path):
+    for file in os.listdir(path):
+        name = file.split('.')
+        name = '{}.{}'.format(''.join(name[:-1]), name[-1])
+        os.rename('{}{}'.format(path, file), '{}{}'.format(path, name))
+
+foo(images_output_path)
+foo(annotation_output_path)
